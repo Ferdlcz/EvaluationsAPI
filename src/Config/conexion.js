dc21692e5885e81251f0ede0dbd61d01102c47fa
Ferdlcz <ferdlcskate@gmail.com>
2023-09-27 00:25:03 -0600

    Correccion archivo principal y conexion a db

---
+++ src/Config/conexion.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () =>{
+    try{
+        await mongoose.connect('mongodb://127.0.0.1:27017/EvaluationsAPI',{
+            useNewUrlParser : true,  
+            useUnifiedTopology:true
+        });
+        console.log('La conexion a la base de datos ha sido establecida correctamente');
+    }catch(err){
+        console.log('Error al conectar a la base de datos', err)
+        throw err;
+    }
+}
+
+module.exports = connectDB;
--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
+//DEPENDENCES
 const express = require('express');
 const app = express();
+
+//FILES
+const connectDB = require('./Config/conexion');
+
+
 const PORT = process.env.PORT || 3000;
+connectDB()
+    .then(() =>{
+        console.log('Conexion a la base de datos establecida');
+        app.listen(PORT, () =>{
+            console.log(`Servidor escuchando en el puerto ${PORT}`)
+        })
+    }).catch((err) =>{
+        console.log('Error al conectar a la base de datos')
+    })
 
-app.get('', (req, res) =>{
+
+//ROUTES
+
+
+app.get('/api', (req, res) =>{
     res.send('Hola mundo!!');
-})
-
-app.listen(PORT, () =>{
-    console.log(`Servidor escuchando en el puerto ${PORT}`)
 })
